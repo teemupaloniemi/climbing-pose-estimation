c65f0938102e46cc7ffa40e9fa1c14886a21836c
tealjapa <tealjapa@student.jyu.fi>
2024-05-13 20:06:24 +0300

    nicer user experiance

--- pose.py
+++ pose.py
@@ -1,4 +1,5 @@
 import math
+import os
 import cv2
 import mediapipe as mp
 import numpy as np
@@ -6,8 +7,18 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
 filename = ''
+print("Usage:\n    1. Provide file path. (give 'l' or 'list' to list current directory)\n    2. press 'a' to start tracking\n    3. press 'k' to keep tracked buffer stored\n    4. Clear everything :) by pressing 'c'\n    5. Quit by pressing 'q' or giving 'q' as a filename\n")
 while (filename != 'q'):
     filename = input("Give filename with ending>")
+    
+    if filename == 'l' or filename == 'list':
+        for item in os.listdir('.'):
+            print(item)
+        continue
+
+    if filename == 'q':
+        break
+    
     cogs = []
     handsl = []
     handsr = []
@@ -15,13 +26,15 @@ while (filename != 'q'):
     feetr = []
     keep = False
     perm = False
+
     cap = cv2.VideoCapture(filename)
+    
     with mp_pose.Pose(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as pose:
       while cap.isOpened():
         success, image = cap.read()
-        image = cv2.resize(image, (math.floor(1440/1.5), math.floor(1080/2)), interpolation= cv2.INTER_LINEAR)
+        image = cv2.resize(image, (math.floor(image.shape[1]), math.floor(image.shape[0])), interpolation= cv2.INTER_LINEAR)
         if not success:
           print("Ignoring empty camera frame.")
           # If loading a video, use 'break' instead of 'continue'.
@@ -69,7 +82,6 @@ while (filename != 'q'):
             0.0,   # 31 - left foot index - Excluded, included in foot
             0.0    # 32 - right foot index - Excluded, included in foot
         ]
-        print(sum(weights))
 
         land = False
         if results.pose_landmarks is not None:
@@ -78,6 +90,7 @@ while (filename != 'q'):
             # Initialize sums for each coordinate
             sum_x = sum(landmark.x * weights[i] for i, landmark in enumerate(results.pose_landmarks.landmark))
             sum_y = sum(landmark.y * weights[i] for i, landmark in enumerate(results.pose_landmarks.landmark))
+            sum_z = sum(landmark.z * weights[i] for i, landmark in enumerate(results.pose_landmarks.landmark))
             if perm:
                 handsl.append((results.pose_landmarks.landmark[19].x, results.pose_landmarks.landmark[19].y))
                 handsr.append((results.pose_landmarks.landmark[20].x, results.pose_landmarks.landmark[20].y))
@@ -86,13 +99,14 @@ while (filename != 'q'):
      
             cog_x = sum_x / 100 
             cog_y = sum_y / 100
+            cog_z = sum_z / 100
             
         # Draw the pose annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         shape = np.zeros_like(image, np.uint8)
         shape = cv2.rectangle(shape, (0,0), (image.shape[0]*2, image.shape[1]), (0, 0, 0), -1)
-        alpha = 0.85
+        alpha = 0.75
         out = image.copy()
         out = cv2.addWeighted(shape, alpha, image, 1 - alpha, 0)
         image = out
@@ -101,10 +115,21 @@ while (filename != 'q'):
             results.pose_landmarks,
             mp_pose.POSE_CONNECTIONS,
             landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
-        if cv2.waitKey(1) == ord('a'):
+        
+        if cv2.waitKey(5) == ord('a'):
             perm = True
-        if cv2.waitKey(1) == ord('k'):
+        
+        if cv2.waitKey(5) == ord('k'):
             keep = True
+
+        if cv2.waitKey(5) == ord('c'):
+            handsl = []
+            handsr = []
+            feetl = []
+            feetr = []
+            cogs = []
+            keep = False
+
         if not keep:
             if len(handsr) > 128:
                 handsr.pop(0)
@@ -116,20 +141,24 @@ while (filename != 'q'):
                 feetr.pop(0)
             if len(cogs) > 128:
                 cogs.pop(0)
+       
+        asp = image.shape[0] / image.shape[1]
         if land and perm:
-            cogs.append((cog_x, cog_y))
+            cogs.append((cog_x, cog_y, cog_z))
             for cog in cogs:
-                cv2.circle(image, (math.floor(cog[0] * image.shape[0] * 1.8), math.floor(cog[1] * image.shape[1] / 1.8)), 2, (0,0,255), -1)
+                cv2.circle(image, (math.floor(cog[0] * image.shape[0] / asp), math.floor(cog[1] * image.shape[1] * asp)), 2, (100*cog[2],100*cog[2],1000*cog[2]*128), -1)
             for i in range(len(handsr)):
-                cv2.circle(image, (math.floor(handsl[i][0] * image.shape[0] * 1.8), math.floor(handsl[i][1] * image.shape[1] / 1.8)), 2, (128,255,255), -1)
-                cv2.circle(image, (math.floor(handsr[i][0] * image.shape[0] * 1.8), math.floor(handsr[i][1] * image.shape[1] / 1.8)), 2, (0,255,255), -1)
-                cv2.circle(image, (math.floor(feetl[i][0] * image.shape[0] * 1.8), math.floor(feetl[i][1] * image.shape[1] / 1.8)), 2, (255,128,255), -1)
-                cv2.circle(image, (math.floor(feetr[i][0] * image.shape[0] * 1.8), math.floor(feetr[i][1] * image.shape[1] / 1.8)), 2, (255,0,255), -1)
+                cv2.circle(image, (math.floor(handsl[i][0] * image.shape[0] / asp), math.floor(handsl[i][1] * image.shape[1] * asp)), 2, (64,255,0), -1)
+                cv2.circle(image, (math.floor(handsr[i][0] * image.shape[0] / asp), math.floor(handsr[i][1] * image.shape[1] * asp)), 2, (0,255,0), -1)
+                cv2.circle(image, (math.floor(feetl[i][0] * image.shape[0] / asp), math.floor(feetl[i][1] * image.shape[1] * asp)), 2, (255,64,0), -1)
+                cv2.circle(image, (math.floor(feetr[i][0] * image.shape[0] / asp), math.floor(feetr[i][1] * image.shape[1] * asp)), 2, (255,0,0), -1)
 
-
-        cv2.imshow("iamge", image)
+        if asp < 1:
+            cv2.imshow("iamge", cv2.resize(image, (1000, 600)))
+        if asp >= 1:
+            cv2.imshow("iamge", cv2.resize(image, (600, 1000)))
         
-        if cv2.waitKey(5) & 0xFF == 27:
+        if cv2.waitKey(5) == ord('q'):
           break
 
     cap.release()
